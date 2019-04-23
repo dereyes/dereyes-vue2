dc28dfc116af9c50114b12b038a692d8883fb796
Darin <hello@derey.es>
2019-04-22 21:03:21 -0700

    Start adding project info

--- dereyes/pages/index.vue
+++ dereyes/pages/index.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
-    <h1>Darin E. Reyes is an immersive artist living in San Luis Obispo, CA</h1>
+    <h1>
+      Darin E. Reyes is an immersive artist living in San Luis Obispo, CA
+    </h1>
+    <h1>
+      <span v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </span>
+    </h1>
+    <h1>
+      <span>
+        About
+      </span>
+      <span>
+        Contact
+      </span>
+      <span>
+        Insta
+      </span>
+    </h1>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects.projects
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 4.5vw;
+}
+</style>
---
+++ dereyes/store/projects.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  projects: [
+    {
+      name: 'ShadowPlay'
+    },
+    {
+      name: 'Gato'
+    },
+    {
+      name: 'Breathe California Rebranding'
+    },
+    {
+      name: 'Fishy Flop'
+    },
+    {
+      name: 'Arirang Visualization'
+    },
+    {
+      name: 'DisQuiet Prototype'
+    }
+  ]
+})
